7109aee4ed4f2dc3dad9088a878c41c1d8306f37
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2023-06-04 14:53:11 +0330

    Dart - annotate return type

--- dart/main.dart
+++ dart/main.dart
@@ -2,11 +2,11 @@ void main(List<String> args) {
   print(countChars('This is a piece   of a text!.'));
 }
 
-countChars(String input) {
-  var result = {};
+Map<String, int> countChars(String input) {
+  Map<String, int> result = {};
   input.toLowerCase().replaceAll(' ', '').split('').forEach((c) {
     if (result.keys.contains(c)) {
-      result[c] += 1;
+      result.addEntries(<String, int>{c: result[c]! + 1}.entries);
     } else {
       result[c] = 1;
     }
